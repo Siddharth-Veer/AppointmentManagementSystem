5900143cc614d338aff2f6ea02d9d2d3e57b7b64
Vidhi <vidhuubana0529@gmail.com>
2024-06-04 15:15:54 -0400

    Changes in Appointment Booking Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import AppointmentBooking from './pages/AppointmentBooking';
 import { AppointmentProvider } from './context/AppointmentContext';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Router>
         <Switch>
           <Route path="/" component={HomePage} exact />
+          <Route path="/book-appointment" component={AppointmentBooking} />
         </Switch>
       </Router>
     </AppointmentProvider>
