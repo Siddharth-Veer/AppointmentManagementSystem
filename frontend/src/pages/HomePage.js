69307c25f27e31f9638d65c67a2a76c721c2a297
Raj <rajahir.dp@gmail.com>
2024-06-06 23:36:04 -0400

    Home page JS file created

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,16 +1,18 @@
 import React, { useContext, useEffect } from 'react';
 import Appointment from '../components/Appointment';
 import { AppointmentContext } from '../context/AppointmentContext';
+import AppointmentManagementSystem from '../components/AppointmentManagementSystem';
 
 const HomePage = () => {
   const { appointments, fetchAppointments } = useContext(AppointmentContext);
 
   useEffect(() => {
     fetchAppointments();
-  }, []);
+  }, [fetchAppointments]);
 
   return (
     <div>
+      <AppointmentManagementSystem />
       <h1>Appointments</h1>
       {appointments.map(appointment => (
         <Appointment key={appointment._id} appointment={appointment} />
