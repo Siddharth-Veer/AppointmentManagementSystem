cfab4a683ac70958f681ba3dadb38f8028bf956d
Tanvi <tsutariya1396@gmail.com>
2024-06-11 16:34:52 -0400

    Reset password FE complete.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import AppointmentBooking from './pages/AppointmentBooking';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
 
 import { AppointmentProvider } from './context/AppointmentContext';
 
@@ -16,8 +17,9 @@ const App = () => {
           <Route path="/" element={<HomePage />} exact />
           <Route path="/book-appointment" element={<AppointmentBooking />} />
           <Route path="/SignIn" element={<SignIn />}  />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path="/SignUp" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
         </Routes>
       </AppointmentProvider>
     </Router>
