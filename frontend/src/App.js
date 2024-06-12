8833e7653e7dafb214650bcd6219471c9ed5b861
Raj <rajahir.dp@gmail.com>
2024-06-12 18:43:27 -0400

    HomepageRaj 12-06-2024

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AppointmentBooking from './pages/AppointmentBooking';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-
 import { AppointmentProvider } from './context/AppointmentContext';
 
 const App = () => {
@@ -12,14 +11,14 @@ const App = () => {
     <Router>
       <AppointmentProvider>
         <Routes>
-          <Route path="/" component={HomePage} exact />
+          <Route path="/" element={<HomePage />} exact />
           <Route path="/book-appointment" element={<AppointmentBooking />} />
-          <Route path="/SignIn" element={<SignIn />}  />
+          <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
       </AppointmentProvider>
     </Router>
   );
-};
+}
 
 export default App;
