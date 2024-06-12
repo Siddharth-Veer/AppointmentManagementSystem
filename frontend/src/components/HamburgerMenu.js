d5d726c7ce73f8e93bdddf6bcad3c17991285b52
Veeraswami Nandagopal Siddharth <87278070+Siddharth-Veer@users.noreply.github.com>
2024-06-12 19:50:21 -0400

    Hamburger Menu Created and working 12-06-24

---
+++ frontend/src/components/HamburgerMenu.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Add this line
+import '../css/HamburgerMenu.css';
+
+const HamburgerMenu = ({ isOpen, onClose }) => {
+  return (
+    <div className={`hamburger-menu ${isOpen ? 'open' : ''}`}>
+      <div className="menu-header">
+        <h2>Manage your health better</h2>
+        <button onClick={onClose}>Close</button>
+      </div>
+      <div className="menu-content">
+        <p>Get up-to-date notifications on walk-in clinic wait-times</p>
+        <p>Save time by reserving and managing bookings</p>
+        <Link to="/signup" className="create-account" onClick={onClose}>Create a patient account</Link>
+        <Link to="/signin" onClick={onClose}>Have an account? Sign in here</Link>
+        <hr />
+        <a href="#">Practice Login</a>
+        <a href="#">List Your Practice</a>
+        <a href="#">Practice Pricing</a>
+        <a href="#">About Us</a>
+        <a href="#">Advertise with Us</a>
+        <a href="#">Contact Us</a>
+      </div>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
