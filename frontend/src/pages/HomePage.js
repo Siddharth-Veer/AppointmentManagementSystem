3ac2368c1df8e5ae6aa48442cd78c9f152abd250
Veeraswami Nandagopal Siddharth <87278070+Siddharth-Veer@users.noreply.github.com>
2024-06-13 19:52:25 -0400

    All the branches are merged of Sprint 1

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -19,6 +19,7 @@ const HomePage = () => {
   return (
     <>
       <header>
+        <img src="..\assets\images\logo.png" alt="Medisync Logo" />
         <div className="logo">Medisync</div>
         <nav>
           <ul>
